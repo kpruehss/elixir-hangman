35169c59cb12ec803c953b8e10d71152be58295a
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2020-04-16 22:43:52 +1000

    dict - pick random word from word list

--- dictionary/lib/dictionary.ex
+++ dictionary/lib/dictionary.ex
@@ -1,5 +1,11 @@
 defmodule Dictionary do
-  def load_wordlist do
+  # Pick a random word from the word list
+  def random_word do
+    Enum.random(load_word_list())
+  end
+
+  # Load wordl list from file
+  def load_word_list do
     contents = File.read!("assets/words.txt")
     list = String.split(contents, ~r/\n/)
   end
