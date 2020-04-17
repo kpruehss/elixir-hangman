65dbb79d43c1b86d1465127ff63ddf8e0e773700
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2020-04-17 17:30:22 +1000

    HM - add deps

--- hangman/mix.exs
+++ hangman/mix.exs
@@ -6,6 +6,7 @@ defmodule Hangman.MixProject do
       app: :hangman,
       version: "0.1.0",
       elixir: "~> 1.10",
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
