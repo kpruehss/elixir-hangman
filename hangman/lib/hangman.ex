7b8daa900bf362dd7346f7602edfcbe2e0de1966
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2020-04-18 14:54:54 +1000

    GE - code cleaning

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -2,7 +2,6 @@ defmodule Hangman do
   alias Hangman.Game
 
   defdelegate new_game(), to: Game
-  # defdelegate make_move(game, guess), to: Game
   defdelegate tally(game), to: Game
 
   def make_move(game, guess) do
