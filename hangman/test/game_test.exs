61c1ba8daf0c6f4903f53d56cb4830b0cdb445d8
Karsten Pruehss <Karsten.Pruehss@gmail.com>
2020-04-18 13:00:35 +1000

    GE-move-log add unit tests
    Add logic+tests to process moves

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -18,4 +18,72 @@ defmodule GameTest do
       assert {^game, _} = Game.make_move(game, "x")
     end
   end
+
+  test "First/Second occurrence of letter is not/is already used " do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state == :already_used
+  end
+
+  test "a good guess is recognized" do
+    game = Game.new_game("puckers")
+    {game, _tally} = Game.make_move(game, "p")
+    assert game.game_state == :good_guess
+    assert game.turns_left == 7
+  end
+
+  test "a guessed word is a won game" do
+    moves = [
+      {"p", :good_guess},
+      {"u", :good_guess},
+      {"c", :good_guess},
+      {"k", :good_guess},
+      {"e", :good_guess},
+      {"r", :good_guess},
+      {"s", :won}
+    ]
+
+    game = Game.new_game("puckers")
+
+    fun = fn {guess, state}, game ->
+      {game, _tally} = Game.make_move(game, guess)
+      assert game.game_state == state
+      game
+    end
+
+    Enum.reduce(moves, game, fun)
+  end
+
+  test "bad guess is recognized" do
+    game = Game.new_game("puckers")
+    {game, _tally} = Game.make_move(game, "t")
+    assert game.game_state == :bad_guess
+    assert game.turns_left == 6
+  end
+
+  test "7 wrong guesses is a lost game" do
+    moves = [
+      {"a", :bad_guess, 6},
+      {"b", :bad_guess, 5},
+      {"d", :bad_guess, 4},
+      {"f", :bad_guess, 3},
+      {"t", :bad_guess, 2},
+      {"y", :bad_guess, 1},
+      {"z", :lost, 0}
+    ]
+
+    game = Game.new_game("w")
+
+    fun = fn {guess, state, index}, game ->
+      {game, _tally} = Game.make_move(game, guess)
+      assert game.game_state == state
+
+      assert game.turns_left == index
+      game
+    end
+
+    Enum.reduce(moves, game, fun)
+  end
 end
